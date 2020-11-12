caf1ca67faea2033bac1c982b71b6c72afbf16e2
Avak Pan <avakpan@gmail.com>
2020-11-12 10:53:38 +0800

    v2 complete

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,13 @@
 window.onload = function () {
-  var container = document.getElementById("container");
+  var container = document.querySelector(".container");
   var cellCount = 9;
   var cellSpace = 5;
   var rowCount = Math.sqrt(cellCount);
   var cellWidth = container.offsetHeight / rowCount - cellSpace * 2;
 
-  var isGameStarted = false;
-
   var positionRecords = new Array(cellCount - 1).fill("").map((v, i) => i + 1);
   positionRecords.push("whiteCell");
-  
+
   function getPosition(index) {
     var left = (index % rowCount) * (cellWidth + cellSpace * 2);
     var top = Math.floor(index / rowCount) * (cellWidth + cellSpace * 2);
@@ -25,19 +23,12 @@ window.onload = function () {
       cellDiv.style.lineHeight = cellDiv.style.width = cellDiv.style.height =
         cellWidth + "px";
       cellDiv.style.fontSize = cellWidth * 0.618 + "px";
+      cellDiv.style.margin = cellSpace + "px";
       var position = getPosition(i);
       cellDiv.style.top = position.top + "px";
       cellDiv.style.left = position.left + "px";
       cellDiv.onclick = function () {
-        if (!isGameStarted) {
-          isGameStarted = true;
-          startGame();
-        } else {
-          move(this);
-          // if (isGameFinish()) {
-          //   alert("You Success");
-          // }
-        }
+        move(this);
       };
       container.appendChild(cellDiv);
     }
@@ -67,7 +58,6 @@ window.onload = function () {
     }
   }
 
-  renderRecord(positionRecords);
 
   function startGame() {
     for (var j = 0; j < Math.round(Math.random() * 100 + 100); j++) {
@@ -77,16 +67,8 @@ window.onload = function () {
     }
   }
 
-  function isGameFinish() {
-    if (positionRecords.indexOf("whiteCell") == positionRecords.length - 1) {
-      var tempArr = positionRecords.slice();
-      tempArr.pop();
-      for (var i = 0; i < tempArr.length - 1; i++) {
-        if (tempArr[i] > tempArr[i + 1]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
+  renderRecord(positionRecords);
+
+  var btnStart = document.getElementById("btnStart");
+  btnStart.onclick = startGame;
 };
